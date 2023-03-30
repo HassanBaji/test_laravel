e28be71f8d24c13fd02fefb368d728ee4fca1314
HassanBaji <120788169+HassanBaji@users.noreply.github.com>
2023-03-30 03:43:25 +0300

    wewwe

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>React App</title>
+    </head>
+    <body>
+        <h1>noooooo</h1>
+        <div id="root"></div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+
+</html>
+
